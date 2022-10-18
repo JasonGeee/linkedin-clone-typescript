901a006abf0c7a07b99ca5357208c6a83769505c
Jason Gutierrez <jasongeee@yahoo.com>
2022-10-17 20:57:08 -0700

    fixed redux configuration and deleted unnecessary files from redux template

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counterSlice";
+// import counterReducer from "../features/counterSlice";
+import userReducer from "../features/userSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
   },
 });
 
