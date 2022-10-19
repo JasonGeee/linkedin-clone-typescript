775f5cdb01db12b354d3dc74730c4ac421c01263
Jason Gutierrez <jasongeee@yahoo.com>
2022-10-18 22:15:00 -0700

    cleaned up code and comments

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -1,30 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState, AppThunk } from "../app/store";
-// import { fetchCount } from "./counterAPI";
-
-// export interface CounterState {
-//   value: number;
-//   status: "idle" | "loading" | "failed";
-// }
-
-// const initialState: CounterState = {
-//   value: 0,
-//   status: "idle",
-// };
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-// export const incrementAsync = createAsyncThunk(
-//   "counter/fetchCount",
-//   async (amount: number) => {
-//     const response = await fetchCount(amount);
-//     // The value we return becomes the `fulfilled` action payload
-//     return response.data;
-//   }
-// );
 
 export const userSlice = createSlice({
   name: "user",
@@ -33,10 +8,10 @@ export const userSlice = createSlice({
   },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
     login: (state, action: PayloadAction<any>) => {
       state.user = action.payload;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
     logout: (state) => {
       state.user = null;
     },
